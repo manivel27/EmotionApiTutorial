7938ee0e574886597922e293873ca855c986652b
Ozan Topal <ozantopal@msn.com>
2017-04-19 20:16:22 +0300

    Project Initialized.

---
+++ EmotionApiTutorial/EmotionApiTutorial/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EmotionApiTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+        private async void TakePhoto_Clicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+
+            StoreCameraMediaOptions options = new StoreCameraMediaOptions();
+
+            options.DefaultCamera = CameraDevice.Front;
+
+            var file = await CrossMedia.Current.TakePhotoAsync(options);
+            
+            if (file == null)
+                return;
+
+            var stream = file.GetStream();
+            var result = await MakeRequest(stream);
+            List<ServiceResult> serviceResult = JsonConvert.DeserializeObject<List<ServiceResult>>(result);
+            
+        }
+        
+        static async Task<string> MakeRequest(Stream stream)
+        {
+            var result = string.Empty;
+
+            using (var httpClient = new HttpClient())
+            {
+                // Setup HttpClient
+                httpClient.BaseAddress = new Uri("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
+                httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "{API_KEY_WILL_BE_HERE}");
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+
+                // Setup data object
+                HttpContent content = new StreamContent(stream);
+                content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/octet-stream");
+
+                // Make POST request
+                var response = await httpClient.PostAsync("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize", content);
+
+                // Read response
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+            return result;
+        }
+        
+        public static byte[] ReadStreamFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ EmotionApiTutorial/EmotionApiTutorial/ServiceResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmotionApiTutorial
+{
+
+    public class ServiceResult
+    {
+        public FeelingOfFace[] Faces { get; set; }
+    }
+
+    public class FeelingOfFace
+    {
+        public Facerectangle faceRectangle { get; set; }
+        public Scores scores { get; set; }
+    }
+
+    public class Facerectangle
+    {
+        public int left { get; set; }
+        public int top { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class Scores
+    {
+        public float anger { get; set; }
+        public float contempt { get; set; }
+        public float disgust { get; set; }
+        public float fear { get; set; }
+        public float happiness { get; set; }
+        public float neutral { get; set; }
+        public float sadness { get; set; }
+        public float surprise { get; set; }
+    }
+
+}
